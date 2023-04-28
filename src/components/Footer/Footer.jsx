6faa66bbf40d2c7d02b3a7f0b1da61f3090a1fb5
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-28 20:49:53 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { FooterContainer } from "./Footer.styled";
 const Footer = () => {
     return (
         <FooterContainer>
-            <p>&copy; Created as a Test task for Go IT Tech Interview Preparation Course</p>
+            <p>&copy; Created as a Test task for GoIT Tech Interview Preparation Course</p>
         </FooterContainer>
     )
 }
