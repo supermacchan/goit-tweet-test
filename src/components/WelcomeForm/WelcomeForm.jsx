f8ca8acf4d9af51043f3c63c313c037a934428ee
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-28 19:38:42 +0300

    WelcomeForm

--- src/components/WelcomeForm/WelcomeForm.jsx
+++ src/components/WelcomeForm/WelcomeForm.jsx
@@ -1,12 +1,18 @@
-import { Form } from "./WelcomeForm.styled";
+import { 
+    Form, 
+    MainTitle, 
+    Label,
+    Input,
+    Button
+ } from "./WelcomeForm.styled";
 
 const WelcomeForm = () => {
     return (
         <Form>
-            <h2>Welcome!</h2>
-            <h3>How can we call you?</h3>
-            <input type="text" />
-            <button type="sumbit">Send</button>
+            <MainTitle>Hi there, stranger!</MainTitle>
+            <Label>How can we call you?</Label>
+            <Input type="text" placeholder="Enter your name" />
+            <Button type="sumbit">Send</Button>
         </Form>
     )
 }
--- src/components/WelcomeForm/WelcomeForm.styled.js
+++ src/components/WelcomeForm/WelcomeForm.styled.js
@@ -2,5 +2,70 @@ import styled from 'styled-components';
 
 export const Form = styled.form`
     width: 500px;
-    border: 2px solid red;
+
+    padding: 20px;
+    margin-top: 60px;
+
+    text-align: center;
+`
+
+export const MainTitle = styled.h2`
+    margin: 0 0 22px 0;
+
+    color: var(--dark-accent-text-color);
+    font-size: 32px;
+    font-weight: 700;
+`
+
+export const Label = styled.h3`
+    margin: 0 0 12px 0;
+
+    color: var(--dark-accent-text-color);
+    font-size: 20px;
+    font-weight: 500;
+`
+
+export const Input = styled.input`
+    padding: 12px;
+    margin-right: 5px;
+
+    border-radius: 10px;
+    border: 1px solid var(--dark-accent-text-color);
+
+    background-color: var(--main-btn-color);
+    color: var(--dark-text-color);
+
+    font-family: inherit;
+    font-size: 14px;
+
+    transition: border-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+    &:hover,
+    &:focus {
+        border-color: var(--active-btn-color);
+    }
+`
+
+export const Button = styled.button`
+    padding: 12px 18px;
+
+    border-radius: 10px;
+    border: 1px solid transparent;
+
+    background-color: var(--btn-hover-color);
+    color: var(--light-text-color);
+
+    font-family: inherit;
+    font-size: 14px;
+    font-weight: 500;
+    text-transform: uppercase;
+
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+    &:hover,
+    &:focus {
+        background-color: var(--active-btn-color);
+        color: var(--dark-text-color);
+    }
 `
