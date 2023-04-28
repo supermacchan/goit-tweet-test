739daf415f49107e0a7f98eb9e65e5679a39f904
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-28 14:37:06 +0300

    HomePage markup

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,16 @@
+const HomePage = () => {
+    return (
+        // wrapper with a bg
+        <div>
+            <section>
+                {/* <WelcomeForm /> || <WelcomeMessage /> - depending on auth */}
+            </section>
+            <section>
+                {/* <About /> */}
+            </section>
+            {/* <Footer /> */}
+        </div>
+    )
+}
+
+export default HomePage;
