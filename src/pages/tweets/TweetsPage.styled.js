823da9038798de159087d971c6cfede8f6f2476b
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-28 22:09:14 +0300

    Filter buttons

--- src/pages/tweets/TweetsPage.styled.js
+++ src/pages/tweets/TweetsPage.styled.js
@@ -9,6 +9,7 @@ export const Section = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    padding: 20px 0;
 
     background-image: url(${bg});
     background-repeat: no-repeat;
@@ -29,5 +30,25 @@ export const List = styled.ul`
 `
 
 export const Button = styled.button`
+    cursor: pointer;
+    padding: 12px 10px;
 
+    border: none;
+    border-radius: 10px;
+    box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+
+    font-family: inherit;
+    font-size: 16px;
+    font-weight: 500;
+    text-transform: uppercase;
+
+    background-color: var(--main-btn-color);
+    color: var(--dark-text-color);
+
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+    &:hover,
+    &:focus {
+        background-color: var(--btn-hover-text-color);
+    }
 `
