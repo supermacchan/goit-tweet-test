772b15073de4e26a71c3791e5c4e4916d0ab46f0
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-28 16:08:10 +0300

    TweetCard markup + TweetPage adjustment

--- src/pages/tweets/TweetsPage.jsx
+++ src/pages/tweets/TweetsPage.jsx
@@ -1,6 +1,12 @@
 import Header from "components/Header/Header";
 import Filter from "components/Filter/Filter";
+import TweetCard from "components/TweetCard/TweetCard";
 import Footer from "components/Footer/Footer";
+
+// temp
+import users from "../../temp/mockapi.json";
+
+console.log(users);
 
 const TweetsPage = () => {
     return (
@@ -11,7 +17,17 @@ const TweetsPage = () => {
                 <Filter />
                 {/* tweet cards */}
                 <ul>
-
+                    {users.map(user => {
+                        return (
+                            <TweetCard
+                                key={user.id}
+                                name={user.user}
+                                avatar={user.avatar}
+                                tweets={user.tweets}
+                                followers={user.followers}
+                            />
+                        )   
+                    })}
                 </ul>
                 <button type="button">Load More</button>
             </section>
