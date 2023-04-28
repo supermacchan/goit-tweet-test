1ce10588ac5e43926a151fb0790ce4a99db90a4f
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-28 21:20:57 +0300

    NotFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,14 @@
 import Header from "components/Header/Header";
+import img from "../../images/tweet-card-img.png"
+import { Section, Message } from "./NotFound.styled";
 
 const NotFound = () => {
     return (
-        <section>
+        <Section>
             <Header />
-            <h2>Oops! This page does not exist.</h2>
-        </section>
+            <Message>Oops! This page does not exist.</Message>
+            <img src={img} alt="filler" />
+        </Section>
     )
 }
 
