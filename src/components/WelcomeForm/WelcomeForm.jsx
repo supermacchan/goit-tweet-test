50e09721d1a3be74ca83f5e79b65783f29282cf2
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-29 13:01:56 +0300

    basic form functionality (WelcomeForm)

--- src/components/WelcomeForm/WelcomeForm.jsx
+++ src/components/WelcomeForm/WelcomeForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { 
     Form, 
     MainTitle, 
@@ -7,11 +8,28 @@ import {
  } from "./WelcomeForm.styled";
 
 const WelcomeForm = () => {
+    const [name, setName] = useState('');
+
+    const onFormSubmit = e => {
+        e.preventDefault();
+        console.log(name);
+        reset();
+    }
+
+    const reset = () => {
+        setName('');
+    }
+
     return (
-        <Form>
+        <Form onSubmit={onFormSubmit}>
             <MainTitle>Hi there, stranger!</MainTitle>
             <Label>How can we call you?</Label>
-            <Input type="text" placeholder="Enter your name" />
+            <Input 
+                type="text"
+                placeholder="Enter your name" 
+                value={name}
+                onChange={(e) => setName(e.currentTarget.value)}
+            />
             <Button type="sumbit">Send</Button>
         </Form>
     )
