64bd3ade76c11153c98e0d3501f96f8b59f353f9
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-29 13:31:09 +0300

    Update userSlice.js

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userInitialState = {
+    name: null,
+    auth: false,
+}
+
+export const userSlice = createSlice({
+    name: "user",
+    userInitialState,
+    reducers: {
+        signIn(state, action) {
+            state.name = action.payload;
+            state.auth = true;
+        },
+        logOut(state) {
+            state.name = null;
+            state.auth = false;
+        },
+    },
+});
+
+export const { signIn, logOut } = userSlice.actions;
+export const userReducer = userSlice.reducer;
