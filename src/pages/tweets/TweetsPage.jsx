4e6b224371a0508fac1729da7f1b950b749124a1
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-30 14:43:27 +0300

    Tweets page cleanup

--- src/pages/tweets/TweetsPage.jsx
+++ src/pages/tweets/TweetsPage.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { 
     selectTweets, 
-    // selectTweetsLoading, 
     selectTweetsError, 
     selectFavorites,
     selectFilter
@@ -12,7 +11,6 @@ import { tweetOperations } from "redux/operations";
 import Header from "components/Header/Header";
 import Filter from "components/Filter/Filter";
 import TweetCard from "components/TweetCard/TweetCard";
-// import Loader from "components/Loader/Loader";
 import { 
     Wrapper, 
     Section, 
@@ -24,7 +22,6 @@ import { toast } from "react-toastify";
 
 const TweetsPage = () => {
     const users = useSelector(selectTweets);
-    // const isLoading = useSelector(selectTweetsLoading);
     const error = useSelector(selectTweetsError);
     const favorites = useSelector(selectFavorites);
     const filter = useSelector(selectFilter);
@@ -44,6 +41,20 @@ const TweetsPage = () => {
         }
     });
 
+    // ===== First Render =====
+    useEffect(() => {
+        const firstRender = async () => {
+            dispatch(setStatusFilter("show all"));
+            const result = await dispatch(tweetOperations.fetchAllTweets({page: 1, itemsPerPage}));
+            setItems(result.payload);
+        };
+
+        firstRender();
+        setPage(1);
+        setMoreAvailable(true);
+    }, [dispatch, itemsPerPage]);
+
+    // ===== Items per page depending on window resize =====
     useEffect(() => {
         const handleWindowResize = () => {
             const width = window.innerWidth;
@@ -59,6 +70,7 @@ const TweetsPage = () => {
             return () => window.removeEventListener('resize', handleWindowResize);
     }, []);
 
+    // ===== Fetch tweets depending on the Filter status =====
     const filterCheck = async () => {
         switch (filter) {
             case "show all": {
@@ -67,14 +79,10 @@ const TweetsPage = () => {
             }
             case "follow": {
                 const result = await fetchNotFollowed(page + 1);
-                console.log(result);
                 return result;
             }
             case "followings": {
                 const result = await fetchFollowed(page + 1);
-                console.log("switch");
-                console.log(result);
-                console.log(items);
                 return result;
             }
             default:
@@ -82,6 +90,49 @@ const TweetsPage = () => {
         }
     };
 
+    // ===== Fetch all tweets =====
+    const fetchAll = async (page) => {
+        const result = await dispatch(tweetOperations.fetchAllTweets({page, itemsPerPage}));
+        if (!page) {
+            setItems(result.payload);
+        }
+        setMoreAvailable(true);
+        return result.payload;
+    }
+
+    // ===== Fetch users that you already follow =====
+    const fetchFollowed = async (page) => {
+        const result = await dispatch(tweetOperations.fetchFollowed({favorites, page, itemsPerPage}));
+        if (!page) {
+            setItems(result.payload);
+        }
+
+        if (result.payload.length < itemsPerPage) {
+            setMoreAvailable(false);
+        } else {
+            setMoreAvailable(true);
+        }
+
+        return result.payload;
+    }
+
+    // ===== Fetch users that you don't follow =====
+    const fetchNotFollowed = async (page) => {
+        const result = await dispatch(tweetOperations.fetchNotFollowed({favorites, page, itemsPerPage}));
+        if (!page) {
+            setItems(result.payload);
+        }
+
+        if (result.payload.length < itemsPerPage) {
+            setMoreAvailable(false);
+        } else {
+            setMoreAvailable(true);
+        }
+
+        return result.payload;
+    }
+
+    // ===== upon Load More button click =====
     const handleLoadMore = async () => {
         const result = await filterCheck();
             
@@ -91,9 +142,7 @@ const TweetsPage = () => {
             setItems(prevState => {
                 return [...prevState, ...result];
             })
-            console.log("finally");
-            console.log(result);
-            console.log(items);
+
             return;
         }
 
@@ -112,91 +161,7 @@ const TweetsPage = () => {
         setMoreAvailable(false);
         toast("Looks like you've reached the end of the list");
     };
-        // const result = await fetchAll(page + 1);
 
-        // if(result.length >= 6) {
-        //     setPage((prevState => prevState + 1));
-        //     setMoreAvailable(true);
-        //     setItems(prevState => {
-        //         return [...prevState, ...result];
-        //     })
-
-        //     return;
-        // }
-        
-        // setPage(1);
-        // setMoreAvailable(false);
-        // toast("Looks like you've reached the end of the list");
-    // }
-
-    // reset page num to 1
-    // useEffect(() => {
-    //     return () => { setPage(1) }
-    // }, []);
-
-    // first render???
-    useEffect(() => {
-        const firstRender = async () => {
-            dispatch(setStatusFilter("show all"));
-            const result = await dispatch(tweetOperations.fetchAllTweets({page: 1, itemsPerPage}));
-            setItems(result.payload);
-        };
-        firstRender();
-        // dispatch(tweetOperations.fetchAllTweets({itemsPerPage}));
-        setPage(1);
-        setMoreAvailable(true);
-    }, [dispatch, itemsPerPage]);
-
-    // useEffect(() => {
-    //     console.log(page);
-    //     console.log(items);
-    //     console.log(users);
-    //     dispatch(tweetOperations.fetchAllTweets({page, itemsPerPage}));
-    // }, [dispatch, page, itemsPerPage]);
-
-    const fetchAll = async (page) => {
-        const result = await dispatch(tweetOperations.fetchAllTweets({page, itemsPerPage}));
-        if (!page) {
-            setItems(result.payload);
-        }
-        setMoreAvailable(true);
-        return result.payload;
-    }
-
-    const fetchFollowed = async (page) => {
-        const result = await dispatch(tweetOperations.fetchFollowed({favorites, page, itemsPerPage}));
-        if (!page) {
-            setItems(result.payload);
-        }
-        // setItems(result.payload);
-        if (result.payload.length < itemsPerPage) {
-            setMoreAvailable(false);
-        } else {
-            setMoreAvailable(true);
-        }
-
-        // setMoreAvailable(true);
-        console.log("fetch followed");
-        console.log(result.payload);
-        console.log(items);
-        return result.payload;
-    }
-
-    const fetchNotFollowed = async (page) => {
-        const result = await dispatch(tweetOperations.fetchNotFollowed({favorites, page, itemsPerPage}));
-        if (!page) {
-            setItems(result.payload);
-        }
-
-        if (result.payload.length < itemsPerPage) {
-            setMoreAvailable(false);
-        } else {
-            setMoreAvailable(true);
-        }
-        // setItems(result.payload);
-        // setMoreAvailable(true);
-        return result.payload;
-    }
 
     return (
         <Wrapper>
@@ -207,7 +172,7 @@ const TweetsPage = () => {
                     fetchFollowed={fetchFollowed}
                     fetchNotFollowed={fetchNotFollowed}
                 />
-                {/* {isLoading && <Loader />} */}
+
                 {error && <Error>Oops! Something went wrong.</Error>}
 
                 <List>
