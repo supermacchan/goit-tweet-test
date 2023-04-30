09362e7c1fb400d9d55b385b6313c99686db23d7
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-30 12:02:13 +0300

    filter: active buttons

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,19 +2,74 @@ import { useSelector, useDispatch } from "react-redux";
 import { statusFilters } from "redux/constants";
 import { setStatusFilter } from "redux/slices/filterSlice";
 import { selectFilter } from "redux/selectors";
-import { List, Button } from "./Filter.styled";
+import { List, Button, ActiveButton } from "./Filter.styled";
 
 const Filter = ({fetchAll, fetchFollowed, fetchNotFollowed}) => {
     const dispatch = useDispatch();
     const filter = useSelector(selectFilter);
+    const filterOptions = Object.values(statusFilters);
 
     const handleFilterChange = (filter) => {
         dispatch(setStatusFilter(filter));
     }
 
+    const fetchCards = (e) => {
+        switch (e.currentTarget.value) {
+            case "show all": {
+                fetchAll();
+                break;
+            }
+            case "follow": {
+                fetchNotFollowed();
+                break;
+            }
+            case "followings": {
+                fetchFollowed();
+                break;
+            }
+            default:
+                return;
+        }
+    }
+
     return (
         <List>
-            <li>
+            {filterOptions.map(option => {
+                if (option === filter) {
+                    return (
+                        <li key={option}>
+                            <ActiveButton 
+                                type="button"
+                                value={option}
+                                selected={filter === statusFilters[option]}
+                                onClick={(e) => {
+                                    handleFilterChange(e.currentTarget.value);
+                                    fetchCards(e);
+                                }}
+                            >
+                                {option}
+                            </ActiveButton>
+                        </li>
+                    )
+                }
+
+                return (
+                    <li key={option}>
+                        <Button 
+                            type="button"
+                            value={option}
+                            selected={filter === statusFilters[option]}
+                            onClick={(e) => {
+                                handleFilterChange(e.currentTarget.value);
+                                fetchCards(e);
+                            }}
+                        >
+                            {option}
+                        </Button>
+                    </li>
+                )
+            })}
+            {/* <li>
                 <Button 
                     type="button"
                     selected={filter === statusFilters.all}
@@ -22,6 +77,7 @@ const Filter = ({fetchAll, fetchFollowed, fetchNotFollowed}) => {
                         handleFilterChange(statusFilters.all);
                         fetchAll();
                     }}
+                    style={isActive && {backgroundColor: "#fff"}}
                 >
                     show all
                 </Button>
@@ -49,7 +105,7 @@ const Filter = ({fetchAll, fetchFollowed, fetchNotFollowed}) => {
                 >
                     followings
                 </Button>
-            </li>
+            </li> */}
         </List>
     )
 }
